b7a0da7b6866473bbb1aa421004bc6434c2a8dc7
Franck Pachot <mail@pachot.net>
2025-11-11 22:42:19 +0100

    run all 3 databases

---
+++ bench.js
@@ -0,0 +1,51 @@
+db.accounts.ensureIndex({  
+  "category": 1,  
+  "operations.date": 1,  
+  "operations.amount": 1  
+});  
+
+function insert(num) {
+  const ops = [];
+  for (let i = 0; i < num; i++) {
+    const account  = Math.floor(Math.random() * 10_000_000) + 1;
+    const category = Math.floor(Math.random() * 3);
+    const operation = {
+      date: new Date(),
+      amount: Math.floor(Math.random() * 1000) + 1,
+    };
+    ops.push({
+      updateOne: {
+        filter: { _id: account },
+        update: {
+          $set: { category: category },
+          $push: { operations: operation },
+        },
+        upsert: true,
+      }
+    });
+  }
+  db.accounts.bulkWrite(ops);
+}
+
+function query(category) {
+  return db.accounts.find(
+    { category: category },
+    { "operations.amount": 1 , "operations.date": 1 }
+  ).sort({ "operations.date": -1 })
+   .limit(1);
+}
+
+let time1 = Date.now(); insert(1000);              let time2 = Date.now()
+let time3 = Date.now(); print(query(1).toArray()); let time4 = Date.now()
+
+const host = db.getMongo()._uri.replace(/^mongodb:\/\/(?:[^@]+@)?([^/]+)\/.*$/, "$1").padEnd(30, ' ');
+const insertTime = String(time2 - time1).padStart(6, ' ');
+const queryTime  = String(time4 - time3).padStart(6, ' ');  
+  
+print(`Host: ${host} Insert elapsed: ${insertTime} ms Query elapsed: ${queryTime} ms`);  
+
+
+//print(`Host: ${db.getMongo()._uri.replace(/^mongodb:\/\/(?:[^@]+@)?([^/]+)\/.*$/, "$1")} Insert elapsed: ${time2 - time1} ms Query elapsed: ${time4 - time3} ms`);  
+
+
+
